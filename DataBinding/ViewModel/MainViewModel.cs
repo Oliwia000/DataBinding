0973f9ce9919d62a60d206a210f8b8182294d767
teren <teren@KOMP>
2025-03-09 12:57:46 +0100

    .

--- DataBinding/ViewModel/MainViewModel.cs
+++ DataBinding/ViewModel/MainViewModel.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
-
-
+using Microsoft.Maui.Controls;
 
 namespace DataBinding.ViewModel
 {
-
     public class MainViewModel
     {
         public ObservableCollection<Person> People { get; set; }
@@ -19,25 +12,27 @@ namespace DataBinding.ViewModel
         public MainViewModel()
         {
             People = new ObservableCollection<Person>
-        {
-            new Person { Name = "Jan Kowalski" },
-            new Person { Name = "Anna Nowak" },
-            new Person { Name = "Julia Pomidor" },
-            new Person { Name = "Tomasz Kaczyński" }
-        };
+            {
+                new Person { Name = "Jan Kowalski" },
+                new Person { Name = "Anna Nowak" },
+                new Person { Name = "Julia Pomidor" },
+                new Person { Name = "Tomasz Kaczyński" }
+            };
 
             ShowPersonCommand = new Command<string>(ShowPerson);
         }
 
         private async void ShowPerson(string name)
         {
-            await Application.Current.MainPage.DisplayAlert("Wybrana osoba", $"Witaj, {name}!", "OK");
+            if (Application.Current?.MainPage != null)
+            {
+                await Application.Current.MainPage.DisplayAlert("Wybrana osoba", $"Witaj, {name}!", "OK");
+            }
         }
     }
 
     public class Person
     {
         public string Name { get; set; }
-
     }
 }
